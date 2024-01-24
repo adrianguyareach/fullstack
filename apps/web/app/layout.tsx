eb2ef0da64922f2abd5c40ac8020a3623b7747ca
adrianguyareach <adrian.guya@wezacare.org>
2024-01-24 20:59:09 +0300

    hover effects done

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -21,29 +21,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-      <div>
-        <div>
+      <body  className="text-gray-600 font-body font-nunito">
+      <div className='grid md:grid-cols-3'>
+        <div className=" md:col-span-1">
           <nav>
             <div>
-              <h1>
-                <a href="/">Food Ninja</a>
+              <h1 className=' font-bold uppercase p-4 border-b border-gray-200'>
+                <a href="/" className=" text-green-500">Food Ninja</a>
               </h1>
             </div>
             <ul>
-              <li>
+              <li className="text-gray-700 font-bold">
                 <a href="#">
                   <span>Home</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                  </svg>
                 </a>
               </li>
               <li>
                 <a href="#">
                   <span>About</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                  </svg>
+
                 </a>
               </li>
               <li>
                 <a href="#">
                   <span>Contact</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                  </svg>
+
                 </a>
               </li>
             </ul>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,44 +4,87 @@ export default function Home() {
 
   
   return (
-    <div>
-      <div>
-        <a href="#">Log in</a>
-        <a href="#">Sign up</a>
+    <div className="px-16 py-16 bg-gray-100 md:col-span-2">
+      <div className=" flex justify-center md:justify-end ">
+        <a href="#" className="btn text-primary border-primary md:border-2 hover:bg-primary hover:text-white">Log in</a>
+        <a href="#" className="btn text-primary ml-2 border-primary md:border-2 hover:bg-primary hover:text-white">Sign up</a>
       </div>
 
       <header>
-        <h2>Recipes</h2>
-        <h3>For Ninjas</h3>
+        <h2 className="text-gray-700 text-6xl font-semibold">Recipes</h2>
+        <h3 className="text-2xl font-semibold">For Ninjas</h3>
       </header>
 
       <div>
-        <h4>Latest Recipes</h4>
+        <h4 className=" font-bold mt-12 border-b border-gray-200">Latest Recipes</h4>
   
-        <div>
+        <div className="mt-8 grid lg:grid-cols-3 gap-5">
           {/* <!-- cards go here --> */}
-          <div> 
+          <div className="card hover:shadow-lg"> 
             <Image src="/stew.jpg" 
                   alt="stew"
                   height={250}
                   width={250}
+                  className=" w-full h-32 sm:h-48"
                   />
-            <div>
-              <span>5 Bean Chili Stew</span>
-              <span>Recipe by Mario</span>
+            <div className="m-4">
+              <span className=" font-bold ">5 Bean Chili Stew</span>
+              <span className=" block text-gray-500 text-sm">Recipe by Mario</span>
+            </div>
+            <div className="badge">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 inline-block">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              <span>25 mins</span>
+            </div>
+          </div>
+          <div className="card hover:shadow-lg"> 
+            <Image src="/noodles.jpg" 
+                  alt="noodles"
+                  height={250}
+                  width={250}
+                  className=" w-full h-32 sm:h-48"
+                  />
+            <div className="m-4">
+              <span className=" font-bold ">Veg noodles</span>
+              <span className=" block text-gray-500 text-sm">Recipe by Mario</span>
+            </div>
+            <div className="badge">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 inline-block">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+              <span>25 mins</span>
+            </div>
+          </div>
+          <div className="card hover:shadow-lg"> 
+            <Image src="/curry.jpg" 
+                  alt="curry"
+                  height={250}
+                  width={250}
+                  className=" w-full h-32 sm:h-48"
+                  />
+            <div className="m-4">
+              <span className=" font-bold ">Tofu Curry</span>
+              <span className=" block text-gray-500 text-sm">Recipe by Mario</span>
+            </div>
+            <div className="badge">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 inline-block">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              <span>25 mins</span>
             </div>
           </div>
         </div>
 
-        <h4>Most Popular</h4>
+        <h4 className=" font-bold mt-12 pb-2 border-b border-gray-200">Most Popular</h4>
 
-        <div>
+        <div className="mt-8">
           {/* <!-- cards go here --> */}
         </div>
       </div>
 
-      <div>
-        <div>Load more</div>
+      <div className=" flex justify-center ">
+        <div className=" btn bg-secondary-100 text-secondary-200 hover:shadow-inner cursor-pointer">Load more</div>
       </div>
     </div>    
     )
